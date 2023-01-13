c8852529dd39b837c166ee81e9e16946469b9d39
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-01-13 10:13:12 +0100

    Add devise sign_out routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   devise_for :users
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
