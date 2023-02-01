8c4c8a4ed0eb83585afd174bf68a9ba582c2f7ca
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-02-01 12:07:52 +0100

    Attach the current user avataer to its profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
 
   has_many :posts
   has_many :comments
+  has_one_attached :avatar
+  before_create :randomize_id
+
+  private
+	
+  def randomize_id
+    begin
+      self.id = SecureRandom.random_number(1_000_000_000)
+    end while User.where(id: self.id).exists?
+  end
 end
