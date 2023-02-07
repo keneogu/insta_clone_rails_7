a369ddb3825a3ac98931470b081a48d175d7f364
Kenechukwu Oguagbaka <kc101com@gmail.com>
2023-02-07 13:21:39 +0100

    Add and Migrate the followability gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         followability
+
   has_many :posts
   has_many :comments
   has_many :likes
